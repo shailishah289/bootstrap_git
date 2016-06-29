c94db6c4710a9e10df90ef5b4294abbadec751d5
shailishah289 <13bce172@nirmauni.ac.in>
2016-06-29 19:41:55 +0530

    added

--- Bootstrap_demo/includes/js/script.js
+++ Bootstrap_demo/includes/js/script.js
@@ -14,4 +14,11 @@ $(function(){
       e.preventDefault();
       $('#successAlert').slideDown();
   });
+
+  $('a.pop').click(function(e)
+  {
+    e.preventDefault();
+  });
+  $('a.pop').popover();
+  $('[rel="tooltip"]').tooltip();
 });
